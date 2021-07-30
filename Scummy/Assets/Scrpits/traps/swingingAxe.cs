0db543217d8f66e1ad6502c3f6f734abd1426309
Tiger1413 <arabsplayer@hotmail.com>
2021-07-31 00:24:42 +0300

    not working swinging axe (changes rotation position when i change speed idk why)

--- Scummy/Assets/Scrpits/traps/swingingAxe.cs
+++ Scummy/Assets/Scrpits/traps/swingingAxe.cs
@@ -4,8 +4,29 @@ using UnityEngine;
 
 public class swingingAxe : MonoBehaviour
 {
+   
+    [SerializeField] private float firstAngle = 45;
+    [SerializeField] private float secondAngle = 145;
+    [SerializeField] private float speed;
+
     private void Update()
     {
-      
+        rotateLeft();
+    }
+    private void rotateLeft()
+    {
+        //transform.Rotate(speed * Time.deltaTime * 100, 0, 0);
+        transform.rotation = Quaternion.Slerp(Quaternion.Euler(145, 0, 0), Quaternion.Euler(45, 0, 0), speed * Time.deltaTime);
+
+        if (transform.rotation.x <= 45)
+            rotateRight();
+    }
+    private void rotateRight()
+    {
+        // transform.Rotate(-speed * Time.deltaTime * 100, 0, 0);
+        transform.rotation = Quaternion.Slerp(Quaternion.Euler(45, 0, 0), Quaternion.Euler(145, 0, 0), speed * Time.deltaTime);
+
+        if (transform.rotation.x >= 145)
+            rotateLeft();
     }
 }
