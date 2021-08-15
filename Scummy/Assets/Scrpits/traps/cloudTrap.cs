7b9e3df3d7d3c78a786c3510745513f4b183c346
Tiger1413 <arabsplayer@hotmail.com>
2021-08-15 17:33:34 +0300

    cloud

---
+++ Scummy/Assets/Scrpits/traps/cloudTrap.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloudTrap : MonoBehaviour
+{
+    public Transform cloud;
+    public GameObject spike;
+    [SerializeField] float minMaxPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                float randomPos = Random.Range(cloud.position.x + minMaxPos, cloud.position.x - minMaxPos);
+                Instantiate(spike, new Vector3(randomPos, cloud.position.y, 0),Quaternion.identity);
+                Debug.Log("SPIKING");
+                Debug.Log(i);
+            }
+        }
+    }
+}
