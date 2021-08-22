b4ee208020ca32266d8dc7628fe2563b7535fc77
Tiger1413 <arabsplayer@hotmail.com>
2021-08-22 16:00:35 +0300

    10% done main menu and fixed saving and loading options :D

--- Scummy/Assets/Scrpits/loadAudioSettings.cs
+++ Scummy/Assets/Scrpits/loadAudioSettings.cs
@@ -4,12 +4,29 @@ using UnityEngine;
 using UnityEngine.Audio;
 public class loadAudioSettings : MonoBehaviour
 {
+    float mastervolume;
+    float sfxvolume;
+    float musicvolume;
+
     public AudioMixer mainMixer;
     void Start()
     {
-        mainMixer.SetFloat("masterVolume", PlayerPrefs.GetFloat("masterVolumevolume"));
-        mainMixer.SetFloat("sfxVolume", PlayerPrefs.GetFloat("sfxVolumevolume"));
-        mainMixer.SetFloat("musicVolume", PlayerPrefs.GetFloat("musicVolumevolume"));
+        SetMixerFloats();
+    }
+
+    void SetMixerFloats()
+    {
+        mastervolume = PlayerPrefs.GetFloat("masterVolumevolume");
+        sfxvolume = PlayerPrefs.GetFloat("sfxVolumevolume");
+        musicvolume = PlayerPrefs.GetFloat("musicVolumevolume");
+
+        print("The volume of " + "mastervolume" + " is : " + mastervolume);
+        print("The volume of " + "sfxVolume" + " is : " + sfxvolume);
+        print("The volume of " + "musicVolume" + " is : " + musicvolume);
+
+        mainMixer.SetFloat("masterVolume",Mathf.Log10(mastervolume) * 20);
+        mainMixer.SetFloat("sfxVolume", Mathf.Log10(sfxvolume) * 20);
+        mainMixer.SetFloat("musicVolume", Mathf.Log10(musicvolume) * 20 );
     }
 
 }
